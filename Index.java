b621a35d6ec44f699145936744b9d3abe6fe265f
mochacup <104591560+pawtals@users.noreply.github.com>
2025-10-17 21:30:54 -0400

    Created classes & UI class

--- Index.java
+++ Index.java
@@ -1,29 +1,8 @@
-import javax.swing.*;
+import classes.UI;
 
-// Main class
-class GFG {
+class Index {
 
-    // Main driver method
     public static void main(String[] args) {
-        // Creating instance of JFrame
-        JFrame frame = new JFrame();
-
-        // Creating instance of JButton
-        JButton button = new JButton("ln");
-
-        // x axis, y axis, width, height
-        button.setBounds(150, 200, 220, 50);
-
-        // adding button in JFrame
-        frame.add(button);
-
-        // 400 width and 500 height
-        frame.setSize(500, 600);
-
-        // using no layout managers
-        frame.setLayout(null);
-
-        // making the frame visible
-        frame.setVisible(true);
+        UI ui = new UI();
     }
 }
---
+++ classes/UI.java
@@ -0,0 +1,12 @@
+package classes;
+
+import javax.swing.*;
+
+public class UI {
+
+    private JFrame frame;
+
+    public UI() {
+        frame = new JFrame();
+    }
+}
