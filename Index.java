786b8706a2feb107350984f6f386af2a7511a9c4
Breaded-Bread <pepperoniman01@gmail.com>
2025-10-17 22:43:37 -0400

    Create label methods and test multiple buttons

--- Index.java
+++ Index.java
@@ -6,10 +6,16 @@ class Index {
 
     public static void main(String[] args) {
         UI ui = new UI();
+        ui.createLabel("Personal Finance Tracker", new Dimension(400, 30));
         ui.createButton(
             "Add Transaction",
-            new Dimension(100, 30),
+            new Dimension(400, 30),
             (e -> System.out.println("Button clicked"))
+        );
+        ui.createButton(
+            "testNewButton",
+            new Dimension(400, 30),
+            (e -> System.out.println("it works"))
         );
     }
 }
--- classes/UI.java
+++ classes/UI.java
@@ -8,10 +8,10 @@ public class UI {
 
     private JFrame frame;
     private JButton button;
+    private JLabel label;
 
     public UI() {
-        frame = new JFrame("TestFrame");
-        button = new JButton("Click Me");
+        frame = new JFrame("Personal Finance Tracker");
         frame.setSize(400, 300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,6 +28,13 @@ public class UI {
         button.addActionListener(action);
         button.setPreferredSize(scale);
         frame.add(button);
+        frame.pack();
+    }
+
+    public void createLabel(String text, Dimension scale) {
+        JLabel label = new JLabel(text);
+        label.setPreferredSize(scale);
+        frame.add(label);
         frame.pack();
     }
 }
