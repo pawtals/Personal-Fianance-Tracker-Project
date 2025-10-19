dc116d186b1237d8e9306d3a92804e521d3536cc
mochacup <104591560+pawtals@users.noreply.github.com>
2025-10-19 07:12:42 -0400

    Init Tree & Stack class logic wrappers

---
+++ classes/Methods/ActionStore.java
@@ -0,0 +1,5 @@
+/*
+ * Stack data with old previous types, undo removes action & restores data.
+ */
+
+public class ActionStore {}
---
+++ classes/Methods/DateSorter.java
@@ -0,0 +1,11 @@
+
+/*
+* see if extends is appropriate
+* Tree struct for dates
+* Sort by days | months | years
+*/
+import classes.Methods.Sorter
+
+public class DateSorter extends Sorter {
+
+}
