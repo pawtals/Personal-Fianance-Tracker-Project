846e305958aa436b06ea941a61eb862973c8caad
mochacup <104591560+pawtals@users.noreply.github.com>
2025-10-18 21:35:11 -0400

    Update Expense.java

--- classes/Expenses/Expense.java
+++ classes/Expenses/Expense.java
@@ -7,10 +7,10 @@ public class Expense {
     // Class for expense object.
     public int amount;
     public String name;
-    public String[] category;
+    public String category;
     public LocalDate date;
 
-    public Expense(int amount, String name, String[] category, LocalDate date) {
+    public Expense(int amount, String name, String category, LocalDate date) {
         this.amount = amount;
         this.name = name;
         this.category = category;
